35a5dbf17943eac8b696b26943c656f42b674200
Takuma Matsunaga <147134447+MatsuTakusan@users.noreply.github.com>
2024-09-06 10:40:19 +0900

    カレンダー

--- app/src/main/java/jp/ac/meijou/projecty/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/projecty/MainActivity.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.projecty;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +9,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import jp.ac.meijou.projecty.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // 日付が選択されたときの処理
+                if (dayOfMonth > 0 && dayOfMonth < 32 ){
+                    var intent = new Intent(this, selectDay.class);
+                    startActivity(intent);
+                }
+                // year, month, dayOfMonth を使用して処理を行う
+                // 予定のTodoリストとかにしてもいいんじゃない？
+            }
         });
     }
 }
---
+++ app/src/main/java/jp/ac/meijou/projecty/selectDay.java
@@ -0,0 +1,37 @@
+package jp.ac.meijou.projecty;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import jp.ac.meijou.projecty.databinding.ActivitySelectDayBinding;
+
+public class selectDay extends AppCompatActivity {
+
+    private ActivitySelectDayBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_select_day);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button.setOnClickListener(view -> {
+
+            //ここに値などを受け取り送るのがいいかと
+            var intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
