fb260d08a8a68a17ad46c83029f0f1a2a3a8395c
Toma Takahashi <231205109@ccmailg.meijo-u.ac.jp>
2024-09-06 11:30:00 +0900

    main エラー改善

--- app/src/main/java/jp/ac/meijou/projecty/selectDay.java
+++ app/src/main/java/jp/ac/meijou/projecty/selectDay.java
@@ -19,7 +19,8 @@ public class selectDay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_select_day);
+        binding = ActivitySelectDayBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
